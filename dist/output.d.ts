9716201fadb3871aab7b2b9a759e0fa2ebe1c98b
CC11001100 <CC11001100@qq.com>
2022-08-04 21:41:08 +0800

    feat: add dist

---
+++ dist/output.d.ts
@@ -0,0 +1,2 @@
+import { Config } from "./config";
+export declare function output(config: Config, payload: string): void;
---
+++ dist/utils.d.ts
@@ -0,0 +1,12 @@
+/**
+ * 把字符串编码为Base64字符串返回
+ *
+ * @param s 要编码的字符串
+ */
+export declare function base64(s: string): string;
+/**
+ * 格式化多行输入，模拟支持数组，主要是为了把 action 传递进来的参数标准化
+ *
+ * @param lines
+ */
+export declare function formatMultiline(lines: string[]): string[];
